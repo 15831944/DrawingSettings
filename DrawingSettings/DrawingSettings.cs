abeccfdb9f15e7287733a2d12f2d63dfa5e2c57e
Владимир Прудников <vladimirsp81@outlook.com>
2016-09-09 18:49:03 +0300

    Начало

--- DrawingSettings/DrawingSettings.cs
+++ DrawingSettings/DrawingSettings.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +10,26 @@ namespace DrawingSettings
 {
     public class DrawingSettings
     {
+        [CommandMethod("Setds")]
+        public void Setds()
+        {
+            // Получаем доступ к чертежу
+            CivilDocument doc = CivilApplication.ActiveDocument;
 
+            // Получаем доступ к параметрам, вкладка "Еденицы измерения и зона"
+            doc.Settings.DrawingSettings.UnitZoneSettings.DrawingUnits = Autodesk.Civil.Settings.DrawingUnitType.Meters;
+            doc.Settings.DrawingSettings.UnitZoneSettings.AngularUnits = Autodesk.Civil.AngleUnitType.Degree;
+            doc.Settings.DrawingSettings.UnitZoneSettings.ImperialToMetricConversion = Autodesk.Civil.Settings.ImperialToMetricConversionType.InternationalFoot;
+            doc.Settings.DrawingSettings.UnitZoneSettings.DrawingScale = 1;
+
+            // Переходим на вкладку "Параметры среды"
+            doc.Settings.DrawingSettings.AmbientSettings.Area.Unit.Value = Autodesk.Civil.AreaUnitType.SquareMeter;
+            doc.Settings.DrawingSettings.AmbientSettings.Area.Precision.Value = 5;
+            doc.Settings.DrawingSettings.AmbientSettings.Area.Rounding.Value = Autodesk.Civil.RoundingType.Normal;
+            doc.Settings.DrawingSettings.AmbientSettings.Area.Sign.Value = Autodesk.Civil.SignType.Always;
+            doc.Settings.DrawingSettings.AmbientSettings.General.SaveCommandChangesToSettings.
+
+        }
 
     }
 }
